6dd05eb89bdef3b900e27156e638c1f5e474b2a5
Rémi Campistron <remi.campistron@gmail.com>
2026-03-27 13:28:35 +0100

    Ajout du décorateur @Service

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.dao.ArticleRepository;
 import com.example.demo.entities.Article;
 import com.example.demo.entities.Category;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ArticleService {
   private final ArticleRepository articleRepository;
   public ArticleService(ArticleRepository articleRepository) {
@@ -12,7 +14,7 @@ public class ArticleService {
   public void updateArticle(Long id, String brand, String description, double price, Category category ) {
 
     Article article = articleRepository.findById(id)
-        .orElseThrow(() -> new RuntimeException("Article not found with id: " + id));
+        .orElseThrow(() -> new RuntimeException("Article introuvable avec l'id : " + id));
 
     article.setBrand(brand);
     article.setDescription(description);
--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 import com.example.demo.dao.CategoryRepository;
 import com.example.demo.entities.Category;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryService {
   private final CategoryRepository categoryRepository;
   public CategoryService(CategoryRepository categoryRepository) {
